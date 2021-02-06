a931929a4f11016eff130398e1a45b82519199db
Marina Solovieva <marishasolovieva@mail.ru>
2021-02-06 22:24:12 +0300

    change db id type to bigint

--- DDL.sql
+++ DDL.sql
@@ -1,6 +1,6 @@
 CREATE TABLE my_db.cities
 (
-    id          int          NOT NULL AUTO_INCREMENT,
+    id          bigint          NOT NULL AUTO_INCREMENT,
     name        VARCHAR(30) UNIQUE,
     description VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
